d403a1ffced25e96b0213a7c440828956e331259
Ridwan Shikder <ridwanshikder101@gmail.com>
2024-03-31 23:23:48 +0600

    #added test data for category #changed bootstrap #added new controller create in category #added new view create in category #added form for category in create

--- Bulkyweb/Data/ApplicationDbContext.cs
+++ Bulkyweb/Data/ApplicationDbContext.cs
@@ -11,5 +11,16 @@ namespace Bulkyweb.Data
         }
 
         public DbSet<Category> Categories { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Category>().HasData(
+
+                new Category { DisplayOrder = 1, Id = 1, Name = "ridan" },
+                new Category { DisplayOrder = 2, Id = 2, Name = "hassan" },
+                new Category { DisplayOrder = 3, Id = 3, Name = "shikder" }
+
+                );
+        }
     }
 }
