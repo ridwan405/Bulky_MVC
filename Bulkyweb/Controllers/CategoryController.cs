5e7b982bfa19399ce6f2b45af939b4f745884e64
Ridwan Shikder <ridwanshikder101@gmail.com>
2024-04-01 16:47:08 +0600

    #added CRUD functionalities #added error messages #added notifications using toastr cdn

--- Bulkyweb/Controllers/CategoryController.cs
+++ Bulkyweb/Controllers/CategoryController.cs
@@ -23,5 +23,109 @@ namespace Bulkyweb.Controllers
         
             return View();
         }
+        [HttpPost]
+        public IActionResult Create(Category obj)
+        {
+
+            if(obj.Name == obj.DisplayOrder.ToString())
+            {
+
+                ModelState.AddModelError("Name", "Category name and Display order cannot be the same");
+            }
+
+            if (obj.Name != null && obj.Name.ToLower() == "test")
+            {
+
+                ModelState.AddModelError("", "test is an invalid name");
+            }
+
+            if (ModelState.IsValid) 
+            {
+
+                _db.Categories.Add(obj);
+                _db.SaveChanges();
+                TempData["success"] = "Category created successfully";
+                return RedirectToAction("Index");
+            }
+           
+            return View();
+            
+        }
+
+        public IActionResult Edit (int? id)
+        { 
+            if(id == null || id == 0) 
+            {
+                TempData["error"] = "operation failed";
+                return NotFound();
+            }
+
+            Category? category = _db.Categories.FirstOrDefault(c => c.Id == id);
+            if (category == null) 
+            {
+                TempData["error"] = "operation failed";
+                return NotFound();
+            }
+
+            return View(category);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Category obj)
+        {
+
+            if (ModelState.IsValid)
+            {
+
+                _db.Categories.Update(obj);
+                _db.SaveChanges();
+                TempData["success"] = "Category updated successfully";
+                return RedirectToAction("Index");
+            }
+
+            return View();
+
+        }
+
+        public IActionResult Delete(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                TempData["error"] = "operation failed";
+                return NotFound();
+            }
+
+            Category? category = _db.Categories.FirstOrDefault(c => c.Id == id);
+            if (category == null)
+            {
+                TempData["error"] = "operation failed";
+                return NotFound();
+            }
+
+            return View(category);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        public IActionResult DeletePost(int? id)
+        {
+
+            Category? category = _db.Categories.FirstOrDefault(u => u.Id == id);
+
+            if (category == null)
+            {
+                TempData["error"] = "operation failed";
+                return NotFound();
+            }
+            else
+            {
+                _db.Categories.Remove(category);
+                _db.SaveChanges();
+                TempData["success"] = "Category deleted successfully";
+            }
+
+
+            return RedirectToAction("Index");
+
+        }
     }
 }
